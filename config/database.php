82623174de5b9dbc24bf501aa12999d097fe6916
Cao Giang Huy <caogianghuy1305@gmail.com>
2025-12-10 16:12:06 +0700

    test
    có thể test nhanh bằng cách mở file database.php

--- config/database.php
+++ config/database.php
@@ -5,12 +5,17 @@ $password = "";
 $dbname = "db_web_du_lich";
 
 // Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
+$connect = new mysqli($servername, $username, $password, $dbname);
 
 // Kiểm tra kết nối
-if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
+if ($connect->connect_error) {
+    // In lỗi ra trang để bạn kiểm tra nhanh (KHÔNG nên in chi tiết lỗi trên production)
+    echo "Kết nối thất bại: " . htmlspecialchars($conn->connect_error);
 }
+
+// Sau nay có thể xóa dòng này đi
+echo "Kết nối DB thành công!";
+
 
 // Thiết lập bộ ký tự cho kết nối
 $conn->set_charset("utf8mb4");
