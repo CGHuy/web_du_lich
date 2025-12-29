1d09d9e2e23b35ecbee149ee08c299a1c2da1faa
Hoang_Anh <kbhoanganh05@gmail.com>
2025-12-29 21:26:17 +0700

    Thêm logic setting user

--- src/views/partials/settings-menu.php
+++ src/views/partials/settings-menu.php
@@ -12,18 +12,20 @@
                     style="color: inherit;">Đổi mật
                     khẩu</a>
             </li>
-            <li class="menu-item <?= $currentPage === 'booking-history' ? 'active' : '' ?>">
-                <i class="fa-solid fa-clock-rotate-left"></i>
-                <a href="<?= route('settinguser.bookingHistory'); ?>" class="text-decoration-none flex-grow-1"
-                    style="color: inherit;">Lịch sử đặt
-                    tour</a>
-            </li>
-            <li class="menu-item <?= $currentPage === 'favorite-tour' ? 'active' : '' ?>">
-                <i class="fa-solid fa-heart"></i>
-                <a href="<?= route('settinguser.favoriteTour'); ?>" class="text-decoration-none flex-grow-1"
-                    style="color: inherit;">Tour yêu
-                    thích</a>
-            </li>
+            <?php if (empty($_SESSION['role']) || $_SESSION['role'] !== 'admin'): ?>
+                <li class="menu-item <?= $currentPage === 'booking-history' ? 'active' : '' ?>">
+                    <i class="fa-solid fa-clock-rotate-left"></i>
+                    <a href="<?= route('settinguser.bookingHistory'); ?>" class="text-decoration-none flex-grow-1"
+                        style="color: inherit;">Lịch sử đặt
+                        tour</a>
+                </li>
+                <li class="menu-item <?= $currentPage === 'favorite-tour' ? 'active' : '' ?>">
+                    <i class="fa-solid fa-heart"></i>
+                    <a href="<?= route('settinguser.favoriteTour'); ?>" class="text-decoration-none flex-grow-1"
+                        style="color: inherit;">Tour yêu
+                        thích</a>
+                </li>
+            <?php endif; ?>
             <li class="menu-item">
                 <i class="fa-solid fa-right-from-bracket"></i>
                 <a href="/web_du_lich/public/logout.php" class="text-decoration-none flex-grow-1"
